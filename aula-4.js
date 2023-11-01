6d9e86746cbc7343175dc8832addb8764b4958ef
Angela Carvalho <angelalopo@outlook.com>
2023-11-01 18:14:33 -0300

    subindo os exercicios da aula 4

---
+++ aula-4.js
@@ -0,0 +1,41 @@
+/*let operacao = (5 + 5)/2;
+console.log(operacao);*/
+
+//Exercicios usando o operador de soma
+
+let numeroSoma = 25;
+let numeroSoma2 = 30;
+let soma = numeroSoma + numeroSoma2;
+
+//console.log("O resultado da soma entre o numero " +numeroSoma+ " e o numero " + numeroSoma2 + " é: " + soma);
+
+let numeroGrande = 1500;
+let numeroGrande2 = 5000;
+let somando = numeroGrande + numeroGrande2;
+
+//console.log(somando)
+
+//Exercício usando o operador de subtração:
+
+//let numero1 = 580;
+//let numero2 = 300;
+//let subtraiNumero = (numero1 - numero2)/2;
+
+//console.log(subtraiNumero);
+
+//Exercício usando o operador de multiplicação;
+
+let numero1 = 5;
+let numero2 = 6;
+
+let multiplicandoNumeros = numero1 * numero2;
+
+//console.log("O resultado da multiplicação é: " + multiplicandoNumeros);
+
+//Exercício usando o operador de divisão;
+
+let dividendo = 20;
+let divisor = 4;
+let divisao = dividendo / divisor;
+
+console.log("O resultado da divisão entre os números é: ", divisao);
---
+++ operadoresDeComparacao.js
@@ -0,0 +1,31 @@
+//Exercício sobre o operador de igualdade ==; 
+
+/*let numero = 5;
+let numero2 = "cinco";
+
+let comparandoDoisNumeros = numero == numero2;
+
+console.log(comparandoDoisNumeros);*/
+
+//Exercício sobre o operador de estrita igualdade ===;
+
+/*let numero = 5;
+let numero2 = "5";
+
+let comparandoDoisNumeros = numero === numero2;
+
+console.log(comparandoDoisNumeros);*/
+
+//Exercício sobre o operador de comparação (não igual ou diferente de) !=;
+
+/*let numero = 20;
+let numero1 = 25;
+let eDiferente = numero != numero1;
+
+console.log(eDiferente);*/
+
+let numero = 25;
+let numero1 = 25;
+let ehDiferente = numero != numero1;
+
+console.log(ehDiferente);

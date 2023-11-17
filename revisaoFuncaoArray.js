32348e464546f22687398f6e4bbe7aa556886c5c
Angela Carvalho <angelalopo@outlook.com>
2023-11-17 18:47:20 -0300

    subindo o exercicio de revisão

---
+++ revisaoFuncaoArray.js
@@ -0,0 +1,32 @@
+/*let nomeCompleto = "Angela Carvalho";
+
+let array = ['Banana', 'Maçã', 'Pera', 'Uva', 'Maracuja'];
+
+console.log(array)
+
+let arrayMisto = ['Banana', 'Maçã', 'Uva', 24, 25, 26];
+
+console.log(arrayMisto.length)
+
+console.log(arrayMisto[1])
+console.log(arrayMisto[4])*/
+
+//Revisão Funções
+
+function mensagem(){
+    console.log("Olá, eu sou uma função")
+}
+
+//console.log(mensagem())
+
+function soma(numero1, numero2){
+    return numero1 + numero2
+}
+
+//console.log(soma(50, 25))
+
+function media(numero1, numero2) {
+    return (numero1 + numero2)/2
+}
+
+console.log(media(50, 25))
